e69ff6ae9a60988b33e73a07983991173816e7b1
Viktor St.one <pistoletviktor@gmail.com>
2022-11-21 01:21:34 +0300

    Learned if and else

---
+++ example_005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+   Console.WriteLine("Ну привет моя дорогая МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
